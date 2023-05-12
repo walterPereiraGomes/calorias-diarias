7c168a8ae3d00234af79f455508dd99452572545
Walter Pereira <walter.neto@youxgroup.com.br>
2023-05-12 18:03:31 -0300

    ajuste

--- src/alimentos.js
+++ src/alimentos.js
@@ -1,172 +1,522 @@
 export const alimentos = [
-    {
-        label: "Peito de frango",
-        value: "chicken breast",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Arroz",
-        value: "rice",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Feijão",
-        value: "bean",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Ovo",
-        value: "egg",
-        unidadeMedida: "unidade",
-    },
-    {
-        label: "Pão",
-        value: "bread",
-        unidadeMedida: "unidade",
-    },
-    {
-        label: "Batara",
-        value: "potato",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Pão integral",
-        value: "whole wheat bread",
-        unidadeMedida: "unidade",
-    },
-    {
-        label: "Batata-doce",
-        value: "sweet potato",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Salmão",
-        value: "salmon",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Atum",
-        value: "tuna",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Sardinha",
-        value: "sardine",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Espinafre",
-        value: "spinach",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Couve",
-        value: "kale",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Pepino",
-        value: "cucumber",
-        unidadeMedida: "unidade",
-    },
-    {
-        label: "Brócolis",
-        value: "broccoli",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Cenoura",
-        value: "carrot",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Tomate",
-        value: "tomato",
-        unidadeMedida: "unidade",
-    },
-    {
-        label: "Abacate",
-        value: "avocado",
-        unidadeMedida: "unidade",
-    },
-    {
-        label: "Banana",
-        value: "banana",
-        unidadeMedida: "unidade",
-    },
-    {
-        label: "Maçã",
-        value: "apple",
-        unidadeMedida: "unidade",
-    },
-    {
-        label: "Laranja",
-        value: "orange",
-        unidadeMedida: "unidade",
-    },
-    {
-        label: "Abacaxi",
-        value: "pineapple",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Melancia",
-        value: "watermelon",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Melão",
-        value: "cantaloupe",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Aveia",
-        value: "oats",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Chia",
-        value: "chia seeds",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Whey Protein",
-        value: "whey protein",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Beterraba",
-        value: "beetroot",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Abóbora",
-        value: "pumpkin",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Açaí",
-        value: "açaí",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Iogurte natural",
-        value: "plain yogurt",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Leite desnatado",
-        value: "skim milk",
-        unidadeMedida: "ml",
-    },
-    {
-        label: "Queijo cottage",
-        value: "cottage cheese",
-        unidadeMedida: "g",
-    },
-    {
-        label: "Queijo branco",
-        value: "white cheese",
-        unidadeMedida: "g",
-    },
+  {
+    label: "Arroz",
+    value: "rice",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Feijão",
+    value: "beans",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Peito de frango",
+    value: "chicken breast",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Carne vermelha magra",
+    value: "lean beef",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Peixe",
+    value: "fish",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Ovos",
+    value: "eggs",
+    unidadeMedida: "unidade",
+  },
+  {
+    label: "Queijo cottage",
+    value: "cottage cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Iogurte grego",
+    value: "greek yogurt",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Leite desnatado",
+    value: "skim milk",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Aveia",
+    value: "oatmeal",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Batata-doce",
+    value: "sweet potato",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Abóbora",
+    value: "pumpkin",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Brócolis",
+    value: "broccoli",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Couve-flor",
+    value: "cauliflower",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Espinafre",
+    value: "spinach",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Cenoura",
+    value: "carrots",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Tomate",
+    value: "tomatoes",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Pepino",
+    value: "cucumber",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Abacate",
+    value: "avocado",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Banana",
+    value: "banana",
+    unidadeMedida: "unidade",
+  },
+  {
+    label: "Maçã",
+    value: "apple",
+    unidadeMedida: "unidade",
+  },
+  {
+    label: "Laranja",
+    value: "orange",
+    unidadeMedida: "unidade",
+  },
+  {
+    label: "Morango",
+    value: "strawberries",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Mirtilo",
+    value: "blueberries",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Framboesa",
+    value: "raspberries",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Amêndoas",
+    value: "almonds",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Nozes",
+    value: "walnuts",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Castanha de caju",
+    value: "cashews",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Sementes de girassol",
+    value: "sunflower seeds",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Sementes de abóbora",
+    value: "pumpkin seeds",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Azeite de oliva",
+    value: "olive oil",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Óleo de coco",
+    value: "coconut oil",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Manteiga de amendoim",
+    value: "peanut butter",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Proteína em pó",
+    value: "protein powder",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Barra de proteína",
+    value: "protein bar",
+    unidadeMedida: "unidade",
+  },
+  {
+    label: "Arroz integral",
+    value: "brown rice",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Pão integral",
+    value: "whole wheat bread",
+    unidadeMedida: "fatia",
+  },
+  {
+    label: "Massa integral",
+    value: "whole wheat pasta",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Quinoa",
+    value: "quinoa",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Lentilha",
+    value: "lentils",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Grão-de-bico",
+    value: "chickpeas",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Tofu",
+    value: "tofu",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Tempeh",
+    value: "tempeh",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Hambúrguer vegetariano",
+    value: "veggie burger",
+    unidadeMedida: "unidade",
+  },
+  {
+    label: "Leite de amêndoas",
+    value: "almond milk",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Leite de soja",
+    value: "soy milk",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Leite de aveia",
+    value: "oat milk",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Leite de arroz",
+    value: "rice milk",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Leite de coco",
+    value: "coconut milk",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Queijo parmesão",
+    value: "parmesan cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo cheddar",
+    value: "cheddar cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo mussarela",
+    value: "mozzarella cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo feta",
+    value: "feta cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo de cabra",
+    value: "goat cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo ricota",
+    value: "ricotta cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo cottage",
+    value: "cottage cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo suíço",
+    value: "swiss cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo provolone",
+    value: "provolone cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo gouda",
+    value: "gouda cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo azul",
+    value: "blue cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo brie",
+    value: "brie cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo camembert",
+    value: "camembert cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo roquefort",
+    value: "roquefort cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo cottage",
+    value: "cottage cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo de cabra",
+    value: "goat cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo ricota",
+    value: "ricotta cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo suíço",
+    value: "swiss cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo provolone",
+    value: "provolone cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo gouda",
+    value: "gouda cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo azul",
+    value: "blue cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo brie",
+    value: "brie cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo camembert",
+    value: "camembert cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Queijo roquefort",
+    value: "roquefort cheese",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Chocolate amargo",
+    value: "dark chocolate",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Cacau em pó",
+    value: "cocoa powder",
+    unidadeMedida: "g",
+  },
+  {
+    label: "Mel",
+    value: "honey",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Molho de tomate",
+    value: "tomato sauce",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Molho de soja",
+    value: "soy sauce",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Molho de pimenta",
+    value: "hot sauce",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Mostarda",
+    value: "mustard",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Ketchup",
+    value: "ketchup",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Vinagre balsâmico",
+    value: "balsamic vinegar",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Vinagre de maçã",
+    value: "apple cider vinegar",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Vinagre de arroz",
+    value: "rice vinegar",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Vinagre branco",
+    value: "white vinegar",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Água",
+    value: "water",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Chá verde",
+    value: "green tea",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Café",
+    value: "coffee",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Suco de laranja",
+    value: "orange juice",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Suco de uva",
+    value: "grape juice",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Suco de maçã",
+    value: "apple juice",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Suco de cranberry",
+    value: "cranberry juice",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Suco de abacaxi",
+    value: "pineapple juice",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Suco de limão",
+    value: "lemon juice",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Suco de tomate",
+    value: "tomato juice",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Água de coco",
+    value: "coconut water",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Cerveja",
+    value: "beer",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Vinho tinto",
+    value: "red wine",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Vinho branco",
+    value: "white wine",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Vodka",
+    value: "vodka",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Whisky",
+    value: "whiskey",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Tequila",
+    value: "tequila",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Rum",
+    value: "rum",
+    unidadeMedida: "ml",
+  },
+  {
+    label: "Gin",
+    value: "gin",
+    unidadeMedida: "ml",
+  },
 ];
